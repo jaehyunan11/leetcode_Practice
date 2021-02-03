39c771b46d9475908324164e166c8375b5b4ae65
jaehyun An <jaehyuna11@gmail.com>
2021-02-02 21:46:46 -0800

    first commit

---
+++ remove_duplicate_from_sorted_array.py
@@ -0,0 +1,6 @@
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2]
+# Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+
+def removeDuplicates(self, nums):
+    pass
