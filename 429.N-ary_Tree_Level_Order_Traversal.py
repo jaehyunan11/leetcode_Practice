d8123b2417d83f844644ea3de7221e2398876bb6
jaehyun An <jaehyuna11@gmail.com>
2021-03-30 16:07:04 -0700

    update

---
+++ 429.N-ary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def levelOrder(self, root: 'Node'):
+        if root is None:
+            return []
+        output = []
+        queue = [root]
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                queue.extend(node.children)
+            result.append(level)
+        return output
