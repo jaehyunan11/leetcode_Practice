9c45031b120e540379fd279236fd82f78f8986ed
jaehyun An <jaehyuna11@gmail.com>
2021-04-05 19:13:16 -0700

    update

---
+++ 1710.Maximum_Units_on_a_truck.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumUnits(self, boxTypes, truckSize):
+        # number of boxes, number of units per box
+        ans = 0
+        boxTypes.sort(reverse=True, key=lambda x: x[1])
+        # loop boxTypes
+        for count, units in boxTypes:
+            if count < truckSize:
+                ans += count * units
+                truckSize -= count
+            else:
+                return ans + truckSize * units
+        return ans
+        # if number of box < trucksize
+        # ans += num boxes * num of unit per box
+        # trucksize -= num boxes
+        # else
+        # return ans + trucksize * number of units per box
+        # return ans
+
+
+S = Solution()
+boxTypes = [[1, 3], [2, 2], [3, 1]]
+truckSize = 4
+print(S.maximumUnits(boxTypes, truckSize))
